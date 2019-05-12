94e584c36438139e805bc8c894553e501538c227
Innocent Onyemaenu <innocent.onyemaenu@lixy.tech>
2019-05-12 23:03:22 +0100

    add login feature specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     sequence(:name) { |n| "test#{n}" }
     sequence(:email) { |n| "test#{n}@test.com" }
-    password '123456'
-    password_confirmation '123456'
+    password { '123456' }
+    password_confirmation { '123456' }
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,14 +6,10 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'database_cleaner'
 require 'capybara/poltergeist'
 require 'factory_bot_rails'
 require 'capybara/rspec'
-
-#config.include Devise::Test::IntegrationHelpers, type: :feature
-#config.include FactoryBot::Syntax::Methods
-Capybara.javascript_driver = :poltergeist
-Capybara.server = :puma
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -66,6 +62,16 @@ RSpec.configure do |config|
   config.after(:each) do
     DatabaseCleaner.clean
   end
+
+  config.include Devise::Test::IntegrationHelpers, type: :feature
+  config.include FactoryBot::Syntax::Methods
+  Capybara.javascript_driver = :poltergeist
+
+  options = {js_errors: false}
+  Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(app, options)
+  end
+  Capybara.server = :puma
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
