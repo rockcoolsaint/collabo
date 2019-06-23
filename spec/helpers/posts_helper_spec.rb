0da360b47f2e32d311fda0e7b4761349b5e1e348
Innocent Onyemaenu <innocent.onyemaenu@lixy.tech>
2019-06-23 17:26:04 +0100

    Add specs for the update_pagination_partial_path helper method

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -65,4 +65,22 @@ RSpec.describe PostsHelper, :type => :helper do
       )
     end
   end
+
+  context '#update_pagination_partial_path' do
+    it "returns an update_pagination partial's path" do
+      posts = double('posts', :next_page => 2)
+      assign(:posts, posts)
+      expect(helper.update_pagination_partial_path).to(
+        eq 'posts/posts_pagination_page/update_pagination'
+      )
+    end
+
+    it "returns a remove_pagination partial's path" do
+      posts = double('posts', :next_page => nil)
+      assign(:posts, posts)
+      expect(helper.update_pagination_partial_path).to(
+        eq 'posts/posts_pagination_page/remove_pagination'
+      )
+    end
+  end
 end
